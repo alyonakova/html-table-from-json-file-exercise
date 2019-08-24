7c4ac9b60a6a272296629bab60a4199cf50e97e7
Alyona Kovalyova <alyona+github@banka.space>
2019-08-24 18:55:48 +0300

    Add functions for creating a table, searching in table and setting columns visibility

---
+++ main.js
@@ -0,0 +1,118 @@
+class Table {
+    constructor(data) {
+        this._data = data;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    set data(value) {
+        this._data = value;
+    }
+
+    show() {
+        document.getElementById("tab").style.display = "block";
+    }
+    createTable(json) {
+        this.show();
+        document.getElementById("tab").append(document.createElement('tbody'));
+        let tbody = document.getElementsByTagName("tbody")[0];
+        for (let key in json) {
+            //создание массива фраз
+            let memoArr = [];
+            for (let k in json[key].memo) {
+                memoArr[k] = json[key].memo[k];
+            }
+            let tr = document.createElement('tr');
+            tr.innerHTML = `<td>${json[key].id}</td>
+             <td>${json[key].name.first}</td>
+             <td>${json[key].name.last}</td>
+             <td>${json[key].gender}</td>
+             <td>${memoArr.join('<br>')}</td>
+             <td><img src="${json[key].img}"></td>`;
+            tbody.append(tr);
+        }
+    }
+}
+
+   async function makeTable() {
+    let response = await fetch('tableData.json');
+    let json =  await response.json();
+    let table = new Table(json);
+    table.createTable(table.data);
+}
+
+function findByWord() {
+    let enteredWord = document.getElementById("search").value;
+    if (enteredWord.trim() === '') {
+        return;
+    }
+    let table = document.getElementById("tab");
+    let reg = new RegExp(enteredWord, 'i');
+    let flag = false;
+    for (let i = 1; i < table.rows.length; i++) {
+        flag = false;
+        for (let j = table.rows[i].cells.length - 1; j >= 0; j--) {
+            flag = reg.test(table.rows[i].cells[j].innerHTML);
+            if (flag) {
+                table.rows[i].cells[j].style.backgroundColor = "yellow";
+            } else {
+                table.rows[i].cells[j].style.backgroundColor = "white";
+            }
+        }
+    }
+}
+
+function setColumnInvisible(colNum, table) {
+    let rowCells = table.rows[0].getElementsByTagName("th");
+    rowCells[colNum].style.display = "none";
+    for (let i = 1; i < table.rows.length; i++) {
+        rowCells = table.rows[i].getElementsByTagName("td");
+        rowCells[colNum].style.display = "none";
+    }
+}
+
+function setColumnVisible(colNum, table) {
+    let rowCells = table.rows[0].getElementsByTagName("th");
+    rowCells[colNum].style.display = "";
+    for (let i = 1; i < table.rows.length; i++) {
+        rowCells = table.rows[i].getElementsByTagName("td");
+        rowCells[colNum].style.display = "";
+    }
+}
+
+function setAllColumnsVisibility() {
+    let table = document.getElementById("tab");
+    if (!document.getElementById("id").checked) {
+        setColumnInvisible(0, table);
+    } else {
+        setColumnVisible(0, table);
+    }
+    if (!document.getElementById("first").checked) {
+        setColumnInvisible(1, table);
+    } else {
+        setColumnVisible(1, table);
+    }
+    if (!document.getElementById("last").checked) {
+       setColumnInvisible(2, table);
+    } else {
+        setColumnVisible(2, table);
+    }
+    if (!document.getElementById("gender").checked) {
+        setColumnInvisible(3, table);
+    } else {
+        setColumnVisible(3, table);
+    }
+    if (!document.getElementById("memo").checked) {
+        setColumnInvisible(4, table);
+    } else {
+        setColumnVisible(4, table);
+    }
+    if (!document.getElementById("img").checked) {
+        setColumnInvisible(5, table);
+    } else {
+        setColumnVisible(5, table);
+    }
+}
+
